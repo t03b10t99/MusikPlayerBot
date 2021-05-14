d838cc2fdbaa6c82f561ffb546ec12faadd1f7f3
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-05-15 05:37:03 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -114,7 +114,7 @@ async def play(_, message: Message):
 
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/00ca4b2a7123c14e56f90.jpg"
+        thumb_name = "https://telegra.ph/file/df331424a557c9bdc1196.mp4"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
