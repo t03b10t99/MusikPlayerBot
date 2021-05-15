b92e09163406845a440ffddb0bc8dc36e14a6d38
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-05-15 08:40:18 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -236,7 +236,7 @@ async def play(_, message: Message):
         await message.reply_photo(
         photo="final.png",
         reply_markup=keyboard,
-        caption=f"🏷 **Judul:** [{title[:60]}]({url})\n**⏱ Durasi:** {duration}\n" \
+        caption=f"🏷 **Judul:** [{title[:60]}]({url})\n**⏱ Durasi musik:** {duration} menit\n" \
                 + f"💡 **Status musik:** memutar\n🎧 **Permintaan:** {requested_by}".format(
         message.from_user.mention()
         ),
