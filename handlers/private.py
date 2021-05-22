6194f09602f6c229cfce63466e04c137986af4af
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-05-23 06:15:20 +0700

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -33,7 +33,6 @@ Tapi Percayalah di setiap tangis pasti akan ada kebahagiaan yang akan datang"
                         "Support 💬", url="https://t.me/GB_03101999"
                     )
             ]
-        ),
      disable_web_page_preview=True
     )
 
