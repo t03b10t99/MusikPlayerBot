e19c27777303ce750a6e450bf4831564fc227cd0
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-05-23 06:29:33 +0700

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -4,25 +4,23 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import BOT_NAME as bn
 from helpers.filters import other_filters2
 
-GROUP_MUSIC_PROBOT_IMG= "https://telegra.ph/file/625c18e0b9335553152ac.jpg"
 
-@Client.on_message(filters.command("start") & filters.private & ~filters.channel)
+@Client.on_message(other_filters2)
 async def start(_, message: Message):
-    await message.reply_photo(GROUP_MUSIC_PROBOT_IMG)
+    await message.reply_sticker("CAACAgUAAxkBAAEFSQABYJyJBCa9zcPfokSy21glVGJ0ZS8AArwCAAIepmlUA2tU7m0u6EkeBA")
     await message.reply_text(
-        f"""👋🏻 Hai {message.from_user.mention}
+        f"""👋🏻 Hai, Saya {bn} 🎵
 
-Saya adalah robot manager grup dan saya juga bisa memainkan musik di obrolan suara di grup anda
-Silahkan baca panduan bot musik [klik disini](https://telegra.ph/GB-MUSIK-BOT-05-12)
-Silahkan tekan > /help untuk melihat fitur manager saya
-━─━─━─━─━─━─━─━─━─━─━
+Bot musik adalah bot sumber terbuka yang memungkinkan Anda memutar musik di grup telegram Anda.
+Tidak mengetahui cara memakainya? Baca ᴘᴀɴᴅᴜᴀɴ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ʙᴏᴛ agar langsung memahami tanpa bertanya!
+─────────────────────
 Kutipan: "Ada waktunya kita akan terpuruk dan menangis.
 Tapi Percayalah di setiap tangis pasti akan ada kebahagiaan yang akan datang"
+Dikelola oleh [ɢᴏᴏᴅ ʙᴏʏs](t.me/GB_03101999)!
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-
                     InlineKeyboardButton(
                         "➗ Tambahkan Ke Grup➗", url="http://t.me/GB_MusikBot?startgroup=start")
                   ],[
@@ -31,22 +29,21 @@ Tapi Percayalah di setiap tangis pasti akan ada kebahagiaan yang akan datang"
                     ),
                     InlineKeyboardButton(
                         "Support 💬", url="https://t.me/GB_03101999"
-                    )
+                    )]
             ]
-
+        ),
      disable_web_page_preview=True
     )
 
-@Client.on_message(filters.command("start@GB_MusikBot") & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**✅ Saya Sudah Online\n👤 Jangan Sampai Lupa Masukin Assistent Saya: @GB_Musik**""",
+      await message.reply_text("""**ɢʙ | ᴍᴜsɪᴋ Player Online ✅**""",
       reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🗒️ Panduan", url="https://telegra.ph/GB-MUSIK-BOT-05-12")
+                        "🎁 ᴅᴏɴᴀsɪ", url="https://saweria.co/DonasiUntukAdmin")
                 ]
             ]
         )
    )
-
