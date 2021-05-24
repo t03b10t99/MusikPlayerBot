805991f179f3d34a9477a0c8abe3bee10cf189d5
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-05-24 08:36:43 +0700

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -51,7 +51,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Diunggah {rok} [ɢᴏᴏᴅ ʙᴏʏs](t.me/GB_03101999)**'
+        rep = '**🎵 Diunggah [ɢᴏᴏᴅ ʙᴏʏs](t.me/GB_03101999)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
