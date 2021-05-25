9bb451d4f7aaad32c82288a253a0c1da6f8a7795
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-05-25 20:25:46 +0700

    Update youtube.py

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -20,7 +20,7 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+            f"**❌ Musik berdurasi lebih dari {DURATION_LIMIT} menit tidak dibolehkan, musik yang disediakan adalah {duration} menit**"
         )
 
     ydl.download([url])
