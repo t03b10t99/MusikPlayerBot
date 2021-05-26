d01fa8aa376699fb678e4bbc68f66167ea193f3c
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-05-26 13:36:59 +0700

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -1,67 +1,101 @@
-import os
-import requests
-import aiohttp
+"""
+RadioPlayerV2, Telegram Voice Chat Userbot
+Copyright (C) 2021  Asm Safone
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+from pyrogram import Client, filters
+
 import youtube_dl
-
-from pyrogram import filters, Client
 from youtube_search import YoutubeSearch
+import requests
 
+import os
+
+## Extra Fns -------------------------------
+
+# Convert hh:mm:ss to seconds
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
 
-@Client.on_message(filters.command('song') & ~filters.private & ~filters.channel)
-def song(client, message):
+## Commands --------------------------------
 
-    user_id = message.from_user.id 
-    user_name = message.from_user.first_name 
-    rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
-
+@Client.on_message(filters.command(['song']))
+def a(client, message):
     query = ''
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('**🔎 Sedang Mencari Musik**')
+    m = message.reply('`Searching... Please Wait...`')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0]['url_suffix']}"
-        #print(results)
-        title = results[0]["title"][:40]       
-        thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f'thumb{title}.jpg'
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, 'wb').write(thumb.content)
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count>0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
+            views = results[0]["views"]
 
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
+            # if time_to_seconds(duration) >= 1800:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
 
-        duration = results[0]["duration"]
-        url_suffix = results[0]["url_suffix"]
-        views = results[0]["views"]
+            performer = f"[ꜱᴀꜰᴏɴᴇ ᴍᴜꜱɪᴄ]" 
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
 
+        except Exception as e:
+            print(e)
+            m.edit('**Found Literary Noting. Please Try Another Song or Use Correct Spelling!**')
+            return
     except Exception as e:
         m.edit(
-            "**❌ Lagu Yang Dicari Tidak Ditemukan\nSilahkan masukkan judul musik dengan lebih detail**"
+            "**Enter Song Name with Command!**"
         )
         print(str(e))
         return
-    m.edit("**🔄 Sedang Mendownload Lagu**")
+    m.edit("`Uploading... Please Wait...`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Diunggah Oleh:** [ɢᴏᴏᴅ ʙᴏʏs](t.me/GB_03101999)'
+        rep = f'**🏷 <b>Judul:**</b> <a href="{link}">{title}</a>\n**⏳ <b>Durasi:**</b> <code>{duration}</code>\n**👀 <b>Penonton:**</b> <code>{views}</code>\n**🎧 <b>Diunggah oleh</b> <a href="https://t.me/GB_MusikBot">ɢʙ | ᴍᴜsɪᴋ ʙᴏᴛ</a>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
+        message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name)
         m.delete()
+        message.delete()
     except Exception as e:
-        m.edit('❌ Error')
+        m.edit('**Terjadi kesalahan. Silakan Laporkan Ini Kepada @GB_03101999 !!**')
         print(e)
-
     try:
         os.remove(audio_file)
         os.remove(thumb_name)
