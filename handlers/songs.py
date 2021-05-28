8de6211229ef3024028861df2c0cca7e5952d165
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-05-28 09:15:10 +0700

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -101,3 +101,74 @@ def a(client, message):
         os.remove(thumb_name)
     except Exception as e:
         print(e)
+
+@Client.on_message(filters.command(["vsong", "video"]))
+async def ytmusic(client,message: Message):
+    global is_downloading
+    if is_downloading:
+        await message.reply_text("Download Sedang Berlangsung, Coba Beberapa Saat Lagi.")
+        return
+
+    urlissed = get_text(message)
+
+    pablo =  await client.send_message(
+            message.chat.id,
+            f"`Mendapatkan {urlissed} Dari Youtube. Mohon Tunggu Sebentar.`")
+    if not urlissed:
+        await pablo.edit("Syntax Tidak Valid, Silahkan Periksa Command <Perintah> Help Untuk Mengetahui Lebih Lanjut.")
+        return
+    
+    search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
+    mi = search.result()
+    mio = mi["search_result"]
+    mo = mio[0]["link"]
+    thum = mio[0]["title"]
+    fridayz = mio[0]["id"]
+    thums = mio[0]["channel"]
+    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    url = mo
+    sedlyf = wget.download(kekme)
+    opts = {
+            "format": "best",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "prefer_ffmpeg": True,
+            "geo_bypass": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}
+            ],
+            "outtmpl": "%(id)s.mp4",
+            "logtostderr": False,
+            "quiet": True,
+        }
+    try:
+        is_downloading = True
+        with youtube_dl.YoutubeDL(opts) as ytdl:
+            infoo = ytdl.extract_info(url, False)
+            duration = round(infoo["duration"] / 240)
+
+            if duration > 240:
+                await pablo.edit(
+                    f"❌ Error. Video Berdurasi Lebih Dari 60 Menit Tidak Diperbolehkan, Video Yang Disediakan Berdurasi {duration} minute(s)"
+                )
+                is_downloading = False
+                return
+            ytdl_data = ytdl.extract_info(url, download=True)
+            
+    
+    except Exception as e:
+        #await pablo.edit(event, f"**Gagal Untuk Mendownload.** \n**❌ Error :** `{str(e)}`")
+        is_downloading = False
+        return
+    
+    c_time = time.time()
+    file_stark = f"{ytdl_data['id']}.mp4"
+    capy = f"**Nama Video ▹** `{thum}` \n**Requested For :** `{urlissed}` \n**Channel :** `{thums}` \n**Link :** `{mo}`"
+    await client.send_video(message.chat.id, video = open(file_stark, "rb"), duration = int(ytdl_data["duration"]), file_name = str(ytdl_data["title"]), thumb = sedlyf, caption = capy, supports_streaming = True , progress=progress, progress_args=(pablo, c_time, f'`Uploading {urlissed} Song From YouTube Music!`', file_stark))
+    await pablo.delete()
+    is_downloading = False
+    for files in (sedlyf, file_stark):
+        if files and os.path.exists(files):
+            os.remove(files)
