cf2aaec672f09a15b74becb86abdb7a920e3f1f7
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-05-28 10:53:18 +0700

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -13,17 +13,27 @@ async def start(_, message: Message):
     await message.reply_text(
         f"""**┗┓ Hai {message.from_user.mention} Nama Saya Adalah {bn} ┏┛
 
-Saya Bot Music Group, Yang Dapat Memutar Musik Di Voice Chat Group Dengan Cara Yang Mudah
-Saya Memiliki Banyak Fitur Praktis Seperti :
-┏━━━━━━━━━━━━━━
-┣• Memutar Musik.
-┣• Mendownload Musik.
-┣• Mencari Musik Yang Ingin Di Putar Atau Di Download.
-┗━━━━━━━━━━━━━━
-❃ Cara Menggunakan: [BACA DISINI](https://telegra.ph/GB-MUSIK-BOT-05-12)
-━━━━━━━━━━━━━━━
-Kutipan: Ada Waktunya Kita Akan Terpuruk Dan Menangis.
-Tapi Percayalah Di Setiap Tangis Pasti Akan Ada Kebahagiaan Yang Akan Datang**
+**Bagaimana Cara Menggunakannya?
+1) Pertama tambahkan bot GB_MusikBot ke grup Anda dan berikan hak admin penuh
+2) Kemudian tambahkan assistent GB_Musik ke grup Anda dan berikan hak admin
+3) Setelah itu ikuti perintah di bawah ini.
+PERINTAH UNTUK SEMUA ANGGOTA GRUP
+• /play - balas url youtube atau file lagu untuk memutar lagu
+• /play [song name] - putar lagu yang Anda minta
+• /song [song name] - unduh lagu yang Anda inginkan dengan cepat
+• /search [song name] - Cari video di youtube dengan detail
+
+PERINTAH UNTUK SEMUA ADMIN GRUP
+• /pause - jeda pemutaran lagu
+• /resume - lanjutkan pemutaran lagu
+• /skip - mainkan lagu berikutnya
+• /end - hentikan pemutaran musik
+
+**📝 CATATAN:
+• Untuk menghindari bot error jangan melakukan spam musik ke dalam antrian sekaligus
+• Musik yang melebihi waktu 1 jam tidak dapat diputar di voice chat
+• Jika userbot tidak mau ke voice chat akhiri obrolan suara dan mulai lagi obrolan suara nya
+• Jika ada masalah silahkan hubungi [ɢᴏᴏᴅ ʙᴏʏs](https://t.me/GB_03101999) Terimakasih**
         """,
         reply_markup=InlineKeyboardMarkup(
             [
