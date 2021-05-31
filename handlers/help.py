bd0c2e9407c682e24742fa95bf94f6561356a1a6
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-05-31 10:40:20 +0700

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -5,11 +5,11 @@ from config import BOT_NAME as bn
 from helpers.filters import other_filters2
 
 
-GROUP_MUSIC_PROBOT_IMG= "https://telegra.ph/file/625c18e0b9335553152ac.jpg"
+GB_MUSIK_BOT_IMG= "https://telegra.ph/file/625c18e0b9335553152ac.jpg"
 
 @Client.on_message(filters.command("help") & filters.private & ~filters.channel)
 async def start(_, message: Message):
-    await message.reply_photo(GROUP_MUSIC_PROBOT_IMG)
+    await message.reply_photo(GB_MUSIK_BOT_IMG)
     await message.reply_text(
         f"""**┗┓ Hai {message.from_user.mention} Nama Saya Adalah {bn} ┏┛**
 
@@ -54,7 +54,25 @@ async def start(_, message: Message):
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**👋🏻 Hai perkenalkan saya ɢʙ | ᴍᴜsɪᴋ ʙᴏᴛ\n👥 Jangan lupa masukin @GB_Musik**""",
+      await message.reply_text("""**Bagaimana Cara Menggunakannya?**
+1) Pertama tambahkan bot @GB_MusikBot ke grup Anda dan berikan hak admin penuh
+2) Kemudian tambahkan assistent @GB_Musik ke grup Anda dan berikan hak admin
+3) Setelah itu ikuti perintah di bawah ini.
+**PERINTAH UNTUK SEMUA ANGGOTA GRUP**
+• /play - balas url youtube atau file lagu untuk memutar lagu
+• /play [song name] - putar lagu yang Anda minta
+• /song [song name] - unduh lagu yang Anda inginkan dengan cepat
+• /search [song name] - Cari video di youtube dengan detail
+**PERINTAH UNTUK SEMUA ADMIN GRUP**
+• /pause - jeda pemutaran lagu
+• /resume - lanjutkan pemutaran lagu
+• /skip - mainkan lagu berikutnya
+• /end - hentikan pemutaran musik
+**📝 CATATAN:
+• Untuk menghindari bot error jangan melakukan spam musik ke dalam antrian sekaligus
+• Musik yang melebihi waktu 1 jam tidak dapat diputar di voice chat
+• Jika userbot tidak mau ke voice chat akhiri obrolan suara dan mulai lagi obrolan suara nya
+• Jika ada masalah silahkan hubungi [ɢᴏᴏᴅ ʙᴏʏs](https://t.me/GB_03101999) Terimakasih**""",
       reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -67,3 +85,4 @@ async def gstart(_, message: Message):
             ]
         )
    )
+
