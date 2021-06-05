cc42423b8073c17d577d3c3ca174663836a731ae
dicyn <71568577+dicyn@users.noreply.github.com>
2021-06-05 19:04:20 +0700

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -79,7 +79,7 @@ def a(client, message):
         )
         print(str(e))
         return
-    m.edit("**🔄 Sedang Mendownload Musik**")
+    m.edit("**🔄 Sedang Mendownload Musik Tunggu Bentar Nyett!!**")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
